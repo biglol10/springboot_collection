c06de39dd8c090939301150a5066a41ab9bbb083
biglol <biglol@Jiwookui-noteubug.local>
2024-03-03 01:01:50 +0900

    event

--- get-in-line/src/main/java/com/biglol/getinline/domain/Event.java
+++ get-in-line/src/main/java/com/biglol/getinline/domain/Event.java
@@ -31,7 +31,7 @@ import lombok.ToString;
 public class Event {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // MySql처럼 auto-increment 담당하는게 IDENTITY. MySql일 때 꼭 확인
     private Long id;
 
     @Setter
@@ -65,16 +65,17 @@ public class Event {
     @Column(nullable = false)
     private Integer capacity;
 
-    @Setter private String memo;
+    @Setter private String memo; // null = true가 기본값이라 @Column안 넣어도 됨
 
     @Column(
             nullable = false,
-            insertable = false,
+            insertable = false, // insert문을 생성시킬 때 column이 추가/포함되지 않는다. JPA를 통해서 insert할 때 column만 뺌
             updatable = false,
-            columnDefinition = "datetime default CURRENT_TIMESTAMP")
-    @CreatedDate
+            columnDefinition = "datetime default CURRENT_TIMESTAMP") // 이건 schema에다가 기본값을 재현하고 싶어서 넣은거
+    @CreatedDate // application level에서 persist하기 전에 영속성 하기 직전에 자동으로 now를 넣어주는 코드를 java코드로 넣은거임. 위랑 목표는 같지만 다른 영역에서 동작. 뺀다면 위를 뺌. 실무에서는 nullable = false 만 넣기도 함
     private LocalDateTime createdAt;
 
+    // 특정한 db기술에 너무 의존하지 않게끔 하고자 하는 목적으로도 ORM을 사용하고 있는건데 columnDefinition에 적은거 보면 MySql문법임. 여기에선 연습을 위해 적은거지만 가능하면 안 적는게 맞음
     @Column(
             nullable = false,
             insertable = false,

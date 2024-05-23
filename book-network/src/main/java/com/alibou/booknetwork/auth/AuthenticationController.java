cd1a9a6dca54f2180c2f241e2e60fce69d56e7fb
biglol <biglol@nate.com>
2024-05-24 01:09:49 +0900

    doing sending email

--- book-network/src/main/java/com/alibou/booknetwork/auth/AuthenticationController.java
+++ book-network/src/main/java/com/alibou/booknetwork/auth/AuthenticationController.java
@@ -1,12 +1,11 @@
 package com.alibou.booknetwork.auth;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
@@ -17,5 +16,10 @@ public class AuthenticationController {
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.ACCEPTED) // what will be the response status by default
-
+    public ResponseEntity<?> register(
+            @RequestBody @Valid RegisterationRequest registerationRequest
+    ) {
+        authenticationService.register(registerationRequest);
+        return ResponseEntity.accepted().build();
+    }
 }
--- book-network/src/main/java/com/alibou/booknetwork/auth/AuthenticationService.java
+++ book-network/src/main/java/com/alibou/booknetwork/auth/AuthenticationService.java
@@ -1,7 +1,75 @@
 package com.alibou.booknetwork.auth;
 
+import com.alibou.booknetwork.role.RoleRepository;
+import com.alibou.booknetwork.user.Token;
+import com.alibou.booknetwork.user.TokenRepository;
+import com.alibou.booknetwork.user.User;
+import com.alibou.booknetwork.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+
+    public void register(RegisterationRequest registerationRequest) {
+        var userRole = roleRepository.findByName("USER")
+                // todo - better exception handling
+                .orElseThrow(() -> new IllegalStateException("ROLE USER was not initialized"));
+
+        var user = User.builder()
+                .firstname(registerationRequest.getFirstname())
+                .lastname(registerationRequest.getLastname())
+                .email(registerationRequest.getEmail())
+                .password(passwordEncoder.encode(registerationRequest.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+
+        userRepository.save(user);
+        sendValidationEmail(user);
+    }
+
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+        // send email
+
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        // generate token
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        // save token
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder stringBuilder = new StringBuilder();
+        // We should use SecureRandom instead of random because it is more secure
+        SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length()); // generate a random index
+            stringBuilder.append(characters.charAt(randomIndex)); // get the character at the random index and append it to the string builder
+        }
+        return stringBuilder.toString();
+    }
 }
